86821e703fef9334cb2d8bce162fe8f9288d2097
Chris Harvey <chrisharvey2pi@gmail.com>
2018-11-29 18:29:16 -0500

    DELETE most of Percentage.class
    leave in some static members

--- src/class/Percentage.class.ts
+++ src/class/Percentage.class.ts
@@ -1,5 +1,3 @@
-import * as xjs from 'extrajs'
-
 // TODO: move to xjs.Number
 const xjs_Number_REGEXP: Readonly<RegExp> = /^-?(?:\d+(?:\.\d+)?|\.\d+)$/
 
@@ -31,36 +29,11 @@ const xjs_Number_REGEXP: Readonly<RegExp> = /^-?(?:\d+(?:\.\d+)?|\.\d+)$/
  * 	- `a * b === b * a`
  * 	- `a * (b * c) === (a * b) * c`
  */
-export default class Percentage extends Number {
-	/** The multiplicative identity of the group of Percentages. */
-	static readonly MULT_IDEN: Percentage = new Percentage(1)
-	/** The multiplicative absorber of the group of Percentages. */
-	static readonly MULT_ABSORB: Percentage = new Percentage()
-
+export default abstract class Percentage {
 	/**
 	 * An immutable RegExp instance, representing a string in Percentage format.
 	 */
 	static readonly REGEXP: Readonly<RegExp> = new RegExp(`^${xjs_Number_REGEXP.source.slice(1,-1)}%$`)
-
-	/**
-	 * Return the maximum of two or more Percentages.
-	 * @param   pcts two or more Percentages to compare
-	 * @returns the greatest of all the arguments
-	 */
-	static max(...pcts: Percentage[]): Percentage {
-		return new Percentage(Math.max(...pcts.map((p) => p.valueOf())))
-		// return pcts.sort((a, b) => (a.lessThan(b)) ? -1 : (b.lessThan(a)) ? 1 : 0).slice(-1)[0]
-	}
-
-	/**
-	 * Return the minimum of two or more Percentages.
-	 * @param   pcts two or more Percentages to compare
-	 * @returns the least of all the arguments
-	 */
-	static min(...pcts: Percentage[]): Percentage {
-		return new Percentage(Math.min(...pcts.map((p) => p.valueOf())))
-		// return pcts.sort((a, b) => (a.lessThan(b)) ? -1 : (b.lessThan(a)) ? 1 : 0)[0]
-	}
 
 	/**
 	 * Parse a string matching {@link Percentage.REGEXP}.
@@ -68,109 +41,22 @@ export default class Percentage extends Number {
 	 * @returns a new Percentage emulating the string
 	 * @throws  {RangeError} if the given string does not match `Percentage.REGEXP`
 	 */
-	static fromString(str: string): Percentage {
+	static fromString(str: string): number {
 		if (!Percentage.REGEXP.test(str)) throw new RangeError(`Invalid string format: '${str}'.`)
 		let numeric_part: number = +str.match(xjs_Number_REGEXP.source.slice(1,-1)) ![0]
-		return new Percentage(numeric_part / 100) // NOTE: implies base 10
-	}
-
-
-	/**
-	 * Construct a new Percentage object.
-	 * @param   p the numeric value of this Percentage
-	 */
-	constructor(p: Percentage|number = 0) {
-		p = p.valueOf()
-		xjs.Number.assertType(p, 'non-negative')
-		xjs.Number.assertType(p, 'finite')
-		super(p)
+		return numeric_part / 100
 	}
 
 	/**
-	 * Get the conjugate of this Percentage.
+	 * Convert a number to a percentage string.
 	 *
-	 * The conjugate of a Percentage is the remaining Percentage required
-	 * to add to a one full percentage (1, or 100%).
-	 *
-	 * This method may only be called on percentages less than or equal to 100%.
-	 * @returns the conjugate of this Percentage
-	 * @throws  {RangeError} if this Percentage is less than 0% or more than 100%
+	 * The number will be multiplied by the square of the radix,
+	 * for example, `toString(0.42)` returns `42%`.
+	 * @param   value the value to stringify
+	 * @param   radix the base of the numerical value
+	 * @returns roughly, `(value * 100).toString() + '%'`
 	 */
-	get conjugate(): Percentage {
-		if (this.valueOf() < 0 || 1 < this.valueOf()) throw new RangeError(`No conjugate exists for ${this.toString()}`)
-		return new Percentage(1 - this.valueOf())
-	}
-
-	/**
-	 * Get the reciprocal of this Percentage.
-	 *
-	 * The reciprocal of a Percentage is its multiplicative inverse:
-	 * the Percentage that when multiplied by this, gives a product of 1 (the multiplicative identity).
-	 * @returns a new Percentage representing the multiplicative inverse
-	 */
-	get reciprocal(): Percentage {
-		return new Percentage(Percentage.MULT_IDEN.valueOf() / this.valueOf())
-	}
-
-
-	/** @override */
-	toString(radix: number = 10): string {
-		return `${(radix**2 * this.valueOf()).toString(radix)}%`
-	}
-
-	/**
-	 * Return whether this Percentage’s value equals the argument’s.
-	 * @param   pct the Percentage to compare
-	 * @returns does this Percentage equal the argument?
-	 */
-	equals(pct: Percentage|number): boolean {
-		return (this === pct) || ((pct instanceof Percentage) ? this.valueOf() === pct.valueOf() : this.equals(new Percentage(pct)))
-	}
-
-	/**
-	 * Return how this Percentage compares to (is less than) another.
-	 * @param   pct  the Percentage to compare
-	 * @returns is this Percentage strictly less than the argument?
-	 */
-	lessThan(pct: Percentage|number): boolean {
-		return (pct instanceof Percentage) ? this.valueOf() < pct.valueOf() : this.lessThan(new Percentage(pct))
-	}
-
-	/**
-	 * Return this Percentage, clamped between two bounds.
-	 *
-	 * This method returns this unchanged iff it is weakly between `min` and `max`;
-	 * it returns `min` iff this is strictly less than `min`;
-	 * and `max` iff this is strictly greater than `max`.
-	 * If `min === max` then this method returns that value.
-	 * If `min > max` then this method switches the bounds.
-	 * @param   min the lower bound
-	 * @param   max the upper bound
-	 * @returns `Percentage.min(Percentage.max(min, this), max)`
-	 */
-	clamp(min: Percentage|number = 0, max: Percentage|number = 1): Percentage {
-		return (min instanceof Percentage && max instanceof Percentage) ?
-			(min.lessThan(max) || min.equals(max)) ? Percentage.min(Percentage.max(min, this), max) : this.clamp(max, min) :
-			this.clamp(new Percentage(min), new Percentage(max))
-	}
-
-	/**
-	 * Multiply this Percentage (the multiplicand) by another (the multiplier).
-	 * @param   multiplier the Percentage to multiply this one by
-	 * @returns a new Percentage representing the product, `multiplicand * multiplier`
-	 */
-	times(multiplier: Percentage|number = 1): Percentage {
-		return (multiplier instanceof Percentage) ?
-			new Percentage(this.valueOf() * multiplier.valueOf()) :
-			this.times(new Percentage(multiplier))
-	}
-
-	/**
-	 * Return the argument scaled by this Percentage.
-	 * @param   x the value to take this Percentage of
-	 * @returns `x`, scaled
-	 */
-	of(x: number): number {
-		return this.valueOf() * x
+	static toString(value: number, radix: number = 10): string {
+		return `${(radix**2 * value).toString(radix)}%`
 	}
 }
